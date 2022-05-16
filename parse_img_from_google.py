71712aee2048b35cfb47c3a4f731c5395ef5a5f9
lifest91 <01stas01@gmail.com>
2022-05-16 15:36:20 +0300

    clear

--- parse_img_from_google.py
+++ parse_img_from_google.py
@@ -7,7 +7,6 @@ import os
 
 
 def get_image_from_google(url, directory):
-    # url = "https://www.google.co.in/search?q="+name+"&source=lnms&tbm=isch"
     headers = {
         "User-Agent":
             "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 Edge/18.19582"
